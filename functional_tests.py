8c9388dea378d8f889613a4f4ce2784416b41ff4
FilgeN <flipeak@gmail.com>
2016-02-24 02:13:14 +0100

    Test funkcjonalny zostal przygotowany wykorzystujac unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,42 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+	"""docstring for NewVisitorTest"""
+	def setUp(self):
+		browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+	def tearDown(self):
+		self.browser.quit()
+		self.browser.implicitly_wait(3)
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		#Edyta dowiedziala sie o nowej, wspanialej aplikacji w postaci listy rzeczy do zrobienia.
+		#Postanowila wiec wejsc na strone glowa tej aplikacji.
+		browser.get('http://localhost:8000')
+		
+		#Zwrocila uwage ze tytul strony i naglowek zawieraja slowo 'Listy'
+		self.assertIn('Listy', in browser.title)
+		self.fail('Zakonczenie testu!')
+		#Od razu jest zachecona, aby wpisac rzeczy do zrobienia
+		
+		#W polu tesktowym wpisala "Kupic pedzle o rozmiarze 8,9" 
+		#( malowanie jest hobby Edyty )
+		
+		#Po nacisnieciu klawisza 'Enter' strona zostala uaktualniona i wyswietla
+		# 1. Kupic pedzle o rozmiarze 8,9 jako element listy rzeczy do zrobienia
+		
+		#Na stronie nadal znajduje sie pole tekstowe zachęcajace do podania kolejnego zadania
+		#Edyta wpisala: "Uzyc pedzli do namalowania pejzazu" (Edyta jest bardzo skrupulatna)
+		
+		#Strona zostala ponownie uaktualniona i teraz wyswietla dwa elementy na liscie rzeczy do zrobienia
+		
+		#Edyta byla ciekawa, czy witryna zapamieta jej liste. Zwrocila uwage na wygenerowany dla niej
+		#unikatowy adres URL, obok ktorego znajduje sie pewien tekst z wyjasnieniem
+		
+		#Przechodzi pod podany adres URL i widzi wyswietlona swoja liste rzeczy do zrobienia
+		
+		#Usatysfakcjonowana kladzie sie spac
+
+if __name__=='__main__':
+	unittest.main(warnings='ignore')
