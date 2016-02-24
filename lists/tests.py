9a3cf8fb7b9030c42f99994f1ac9db82d083c49e
FilgeN <flipeak@gmail.com>
2016-02-24 02:44:42 +0100

    Pierwszy test jednostkowy, mapowanie url i atrapa widoku.

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,11 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from lists.views import home_page
 
 # Create your tests here.
-class SmokeTest(TestCase):
-	"""docstring for SmokeTest"""
-	def test_bad_maths(self):
-		self.assertEqual(1+1,3)
+class HomepAgetest(TestCase):
+	"""docstring for HomepAgetest"""
+	def test_root_url_resolvers_to_home_page_view(self):
+		found = resolve('/')
+		self.assertEqual(found.func, home_page)
 		
